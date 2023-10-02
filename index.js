c55e3a8717b55663629418e322bc804db9ca4d0a
terence <terenceshawn18@outlook.com>
2023-10-02 09:06:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+let welcomeMessage = "Welcome to Flatbook, " + `${currentUser}!`;
+console.log(welcomeMessage);
+
+let excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+console.log(shortGreeting)
